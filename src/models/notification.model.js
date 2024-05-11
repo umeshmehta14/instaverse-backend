71a25f89f6838064200e11a02189de0276c3b015
umeshmehta14 <ishaanmehta782@gmail.com>
2024-05-11 18:05:34 +0530

    add: notification model

---
+++ src/models/notification.model.js
@@ -0,0 +1,23 @@
+import { model, Schema } from "mongoose";
+
+const notificationSchema = new Schema(
+  {
+    userId: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    type: { type: String, enum: ["like", "comment", "follow"], required: true },
+    actionBy: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    post: { type: Schema.Types.ObjectId, ref: "Post" },
+    commentId: { type: Schema.Types.ObjectId, ref: "Comment" },
+    read: { type: Boolean, default: false },
+  },
+  { timestamps: true }
+);
+
+export const notification = model("Notification", notificationSchema);
