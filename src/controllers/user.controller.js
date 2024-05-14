7a43ced3c9b853a9306d5c1ec57d3058cca32463
umeshmehta14 <ishaanmehta782@gmail.com>
2024-05-14 14:22:19 +0530

    fix: to send followers following detail

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -592,6 +592,42 @@ const getFollower = asyncHandler(async (req, res) => {
         as: "follower",
         pipeline: [
           {
+            $lookup: {
+              from: "users",
+              localField: "follower",
+              foreignField: "_id",
+              as: "follower",
+              pipeline: [
+                {
+                  $project: {
+                    username: 1,
+                    _id: 1,
+                    follower: 1,
+                    following: 1,
+                  },
+                },
+              ],
+            },
+          },
+          {
+            $lookup: {
+              from: "users",
+              localField: "following",
+              foreignField: "_id",
+              as: "following",
+              pipeline: [
+                {
+                  $project: {
+                    username: 1,
+                    _id: 1,
+                    follower: 1,
+                    following: 1,
+                  },
+                },
+              ],
+            },
+          },
+          {
             $project: {
               _id: 1,
               "avatar.url": 1,
